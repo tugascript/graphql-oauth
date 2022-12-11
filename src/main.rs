3d7b019211ee1e6ab83b5ff2f55990744f0bd2b3
Afonso Barracha <barracha.afonso@gmail.com>
2022-12-11 15:19:13 +0000

    feature: add actix-web set-up to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+use dotenvy::dotenv;
+use std::env;
+
+use graphql_local_oauth::{
+    app::configure_app,
+    config::{Cache, Database, Jwt, Mailer},
+};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let env_type = env::var("ENV_TYPE").unwrap();
+    let cache = Cache::new();
+    let db = Database::new(&env_type).await;
+    let jwt = Jwt::new();
+    let mailer = Mailer::new();
+    let env_copy = env_type.clone();
+    let port = env::var("PORT").unwrap().parse::<u16>().unwrap();
+
+    HttpServer::new(move || {
+        App::new().configure(configure_app(&cache, &db, &jwt, &mailer, &env_type))
+    })
+    .bind((
+        match env_copy.as_str() {
+            "production" => "0.0.0.0",
+            _ => "127.0.0.1",
+        },
+        port,
+    ))?
+    .run()
+    .await
 }
